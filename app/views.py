fc9a85c4d97493cf8e6dc2388e09cecc6a07885e
KAKAO <KAKAO@kakaoui-MacBook-mhmh.local>
2016-09-23 21:10:41 +0900

    exp1

--- app/views.py
+++ app/views.py
@@ -3,51 +3,67 @@ from app import app
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 # 연습문제
 @app.route('/practice')
 def practice():
-	return render_template('practice.html')
+    return render_template('practice.html')
 
 @app.route('/practice01')
 def practice01():
-	return render_template('practice01.html')
+    return render_template('practice01.html')
 
 @app.route('/practice02')
 def practice02():
-	return render_template('practice02.html')
+    return render_template('practice02.html')
 
 @app.route('/practice02-2')
 def practice02_2():
-	return render_template('practice02-2.html')
+    return render_template('practice02-2.html')
 
 # 실험 끝난 이후 리다이렉트용
 @app.route('/practice_finished')
 def practice_finished():
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
 
 # 실험
 @app.route('/exp1/intro')
 def exp1_intro():
-	return render_template('exp1_intro.html')
+    return render_template('exp1_intro.html')
 
 @app.route('/exp2/intro')
 def exp2_intro():
-	return render_template('exp2_intro.html')
+    return render_template('exp2_intro.html')
 
 @app.route('/exp1/a1/scroll')
 def exp1_a1_scroll():
-	return render_template('a1_scroll.html')
+    return render_template('a1_scroll.html')
 
 @app.route('/exp1/a2/scroll')
 def exp1_a2_scroll():
-	return render_template('a2_scroll.html')
+    return render_template('a2_scroll.html')
 
 @app.route('/exp1/a3/scroll')
 def exp1_a3_scroll():
-	return render_template('a3_scroll.html')
+    return render_template('a3_scroll.html')
 
 @app.route('/exp1/rest')
 def rest():
-	return render_template('rest_scroll.html')
+    return render_template('rest_scroll.html')
+
+@app.route('/exp1/b1/page')
+def exp1_b1_page():
+    return render_template('b1_page.html')
+
+@app.route('/exp1/b2/page')
+def exp1_b2_page():
+    return render_template('b2_page.html')
+
+@app.route('/exp1/b3/page')
+def exp1_b3_page():
+    return render_template('b3_page.html')
+
+@app.route('/exp1/outro')
+def exp1_outro():
+    return render_template('exp1_outro.html')
--- run.py
+++ run.py
@@ -2,16 +2,16 @@ from app import app
 import argparse
 
 def get_args():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', dest='debug', action='store_true', help='Debug Mode')
-	return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', dest='debug', action='store_true', help='Debug Mode')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-	args = get_args()
+    args = get_args()
 
-	if args.debug:
-		print(' * Debug Mode')
-		app.run(host='0.0.0.0', port=5000, debug=True)
-	else:
-		print(' * Production Mode')
-		app.run(host='0.0.0.0', port=5000)
+    if args.debug:
+        print(' * Debug Mode')
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    else:
+        print(' * Production Mode')
+        app.run(host='0.0.0.0', port=5000)
