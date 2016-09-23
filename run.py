45e13d3956683447a96c30229f56c2da62d9786d
KAKAO <KAKAO@kakaoui-MacBook-mhmh.local>
2016-09-23 18:33:50 +0900

    -d로 디버그 모드 진입

--- run.py
+++ run.py
@@ -1,2 +1,17 @@
 from app import app
-app.run()
+import argparse
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-d', dest='debug', action='store_true', help='Debug Mode')
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = get_args()
+
+	if args.debug:
+		print(' * Debug Mode')
+		app.run(host='0.0.0.0', port=5000, debug=True)
+	else:
+		print(' * Production Mode')
+		app.run(host='0.0.0.0', port=5000)
