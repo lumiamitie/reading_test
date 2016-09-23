863ffb29b022a8665d1183846538503af5eed4ba
KAKAO <KAKAO@kakaoui-MacBook-mhmh.local>
2016-09-23 10:13:55 +0900

    add practice

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from app import app
 def index():
 	return render_template('index.html')
 
+# 연습문제
 @app.route('/practice')
 def practice():
 	return render_template('practice.html')
@@ -12,3 +13,20 @@ def practice():
 @app.route('/practice01')
 def practice01():
 	return render_template('practice01.html')
+
+@app.route('/practice02')
+def practice02():
+	return render_template('practice02.html')
+
+@app.route('/practice02-2')
+def practice02_2():
+	return render_template('practice02-2.html')
+
+# 실험
+@app.route('/exp1_intro')
+def exp1_intro():
+	return render_template('exp1_intro.html')
+
+@app.route('/exp2_intro')
+def exp2_intro():
+	return render_template('exp2_intro.html')
