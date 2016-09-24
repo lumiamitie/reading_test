8837e62d1d993e92c8b33b580ed849d45afd568a
KAKAO <KAKAO@kakaoui-MacBook-mhmh.local>
2016-09-24 19:48:02 +0900

    실험3 추가

--- app/views.py
+++ app/views.py
@@ -27,14 +27,17 @@ def practice02_2():
 def practice_finished():
     return redirect(url_for('index'))
 
-# 실험
+# 실험1
+# @app.route('/exp1/intro')
+# def exp1_intro():
+#     return render_template('exp1_intro.html')
+
 @app.route('/exp1/intro')
 def exp1_intro():
-    return render_template('exp1_intro.html')
-
-@app.route('/exp2/intro')
-def exp2_intro():
-    return render_template('exp2_intro.html')
+    test_no = 1
+    test_type = ['스크롤', '페이지']
+    next_page = '/exp1/a1/scroll'
+    return render_template('exp_intro.html', test_no=test_no, test_type=test_type, next_page=next_page)
 
 @app.route('/exp1/a1/scroll')
 def exp1_a1_scroll():
@@ -49,7 +52,7 @@ def exp1_a3_scroll():
     return render_template('a3_scroll.html')
 
 @app.route('/exp1/rest')
-def rest():
+def exp1_rest():
     return render_template('rest_scroll.html')
 
 @app.route('/exp1/b1/page')
@@ -66,4 +69,110 @@ def exp1_b3_page():
 
 @app.route('/exp1/outro')
 def exp1_outro():
-    return render_template('exp1_outro.html')
+    test_no = 1
+    return render_template('exp_outro.html', test_no=test_no)
+
+
+# 실험2
+# @app.route('/exp2/intro')
+# def exp2_intro():
+#     return render_template('exp2_intro.html')
+
+@app.route('/exp2/intro')
+def exp2_intro():
+    test_no = 2
+    test_type = ['페이지', '스크롤']
+    next_page = '/exp2/a1/page'
+    return render_template('exp_intro.html', test_no=test_no, test_type=test_type, next_page=next_page)
+
+@app.route('/exp2/a1/page')
+def exp2_a1_page():
+    next_page = '/exp2/a2/page'
+    return render_template('a1_page.html', next_page=next_page)
+
+@app.route('/exp2/a2/page')
+def exp2_a2_page():
+    next_page = '/exp2/a3/page'
+    return render_template('a2_page.html', next_page=next_page)
+
+@app.route('/exp2/a3/page')
+def exp2_a3_page():
+    next_page = '/exp2/rest'
+    return render_template('a3_page.html', next_page=next_page)
+
+@app.route('/exp2/rest')
+def exp2_rest():
+    next_page = '/exp2/b1/scroll'
+    return render_template('rest_page.html', next_page=next_page)
+
+@app.route('/exp2/b1/scroll')
+def exp2_b1_scroll():
+    next_page = '/exp2/b2/scroll'
+    return render_template('b1_scroll.html', next_page=next_page)
+
+@app.route('/exp2/b2/scroll')
+def exp2_b2_scroll():
+    next_page = '/exp2/b3/scroll'
+    return render_template('b2_scroll.html', next_page=next_page)
+
+@app.route('/exp2/b3/scroll')
+def exp2_b3_scroll():
+    next_page = '/exp2/outro'
+    return render_template('b3_scroll.html', next_page=next_page)
+
+@app.route('/exp2/outro')
+def exp2_outro():
+    test_no = 2
+    return render_template('exp_outro.html', test_no=test_no)
+
+
+# 실험3
+@app.route('/exp3/intro')
+def exp3_intro():
+    test_no = 3
+    test_type = ['스크롤', '페이지']
+    next_page = '/exp3/b1/scroll'
+    return render_template('exp_intro.html', test_no=test_no, test_type=test_type, next_page=next_page)
+
+# 지문 B (스크롤)
+@app.route('/exp3/b1/scroll')
+def exp3_b1_scroll():
+    next_page = '/exp3/b2/scroll'
+    return render_template('b1_scroll.html', next_page=next_page)
+
+@app.route('/exp3/b2/scroll')
+def exp3_b2_scroll():
+    next_page = '/exp3/b3/scroll'
+    return render_template('b2_scroll.html', next_page=next_page)
+
+@app.route('/exp3/b3/scroll')
+def exp3_b3_scroll():
+    next_page = '/exp3/rest'
+    return render_template('b3_scroll.html', next_page=next_page)
+
+# 휴식
+@app.route('/exp3/rest')
+def exp3_rest():
+    next_page = '/exp3/a1/page'
+    return render_template('rest_page.html', next_page=next_page)
+
+# 지문 A (페이지)
+@app.route('/exp3/a1/page')
+def exp3_a1_page():
+    next_page = '/exp3/a2/page'
+    return render_template('a1_page.html', next_page=next_page)
+
+@app.route('/exp3/a2/page')
+def exp3_a2_page():
+    next_page = '/exp3/a3/page'
+    return render_template('a2_page.html', next_page=next_page)
+
+@app.route('/exp3/a3/page')
+def exp3_a3_page():
+    next_page = '/exp3/outro'
+    return render_template('a3_page.html', next_page=next_page)
+
+@app.route('/exp3/outro')
+def exp3_outro():
+    test_no = 3
+    return render_template('exp_outro.html', test_no=test_no)
